aa0ad4a79dc998ccd01bfe487075b69ebe4f238d
rafin8990 <rafin8990@gmail.com>
2023-02-11 10:16:49 +0600

    user-admin server created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const port=process.env.PORT || 5000 ;
+require('dotenv').config();
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send('admin user server is running')
+});
+
+app.listen(port, ()=>{
+    console.log(`admin user server is running on port ${port}`)
+});
