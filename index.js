ac6018b9b99f56682bfb82711c7f1ae70d06ac42
rafin8990 <rafin8990@gmail.com>
2023-02-11 11:11:15 +0600

    mongodb server added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const cors=require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express();
 const port=process.env.PORT || 5000 ;
 require('dotenv').config();
@@ -7,6 +8,22 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nuouh7o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ async function run (){
+    try{
+
+    }
+    finally{
+
+    }
+ }
+ run().catch(error=>console.error(error))
+
 
 app.get('/', (req, res)=>{
     res.send('admin user server is running')
